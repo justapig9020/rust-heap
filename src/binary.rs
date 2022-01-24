f738e2f475b355dae385cd12fc45fc470164ee0f
justapig9020 <justapig9020@gmail.com>
2022-01-24 12:13:59 +0800

    Impl min binary heap

--- src/binary.rs
+++ src/binary.rs
@@ -6,10 +6,25 @@ pub struct BinaryHeap<T: Ord> {
 
 impl <T: Ord> Heap<T> for BinaryHeap<T> {
     fn push(&mut self, val: T) -> HeapResult {
+        self.heap.push(val);
+        self.heapify_button_up();
         Ok(())
     }
     fn pop(&mut self) -> Option<T> {
-        None
+        let n = self.heap.len();
+        if n == 0 {
+            None
+        } else if n == 1 {
+            self.heap.pop()
+        } else {
+            self.heap.swap(0, n - 1);
+            let ret = self.heap.pop();
+            self.heapify_top_button();
+            ret
+        }
+    }
+    fn is_empty(&self) -> bool {
+        self.heap.is_empty()
     }
 }
 
@@ -24,4 +39,59 @@ impl <T: Ord> BinaryHeap<T> {
             heap: vec![]
         }
     }
+    fn heapify_button_up(&mut self) {
+        let heap = &mut self.heap;
+        let mut curr = heap.len() - 1;
+        while curr > 0 {
+            let parent = parent_of(curr);
+            if heap[parent] > heap[curr] {
+                heap.swap(parent, curr);
+                curr = parent;
+            } else {
+                break;
+            }
+        }
+    }
+    fn heapify_top_button(&mut self) {
+        let mut curr = 0;
+        let last = self.heap.len() - 1;
+        if last == 0 {
+            return;
+        }
+        let last_parent = parent_of(last);
+        while curr <= last_parent {
+            let lchild = lchild_of(curr);
+            let rchild = rchild_of(curr);
+            let to_check = if rchild > last {
+                lchild
+            } else {
+                if self.should_heigher(lchild, rchild) {
+                    lchild
+                } else {
+                    rchild
+                }
+            };
+            if !self.should_heigher(curr, to_check) {
+                self.heap.swap(curr, to_check);
+                curr = to_check;
+            } else {
+                break;
+            }
+        }
+    }
+    /// Check whether "i" should be heigher than "j"
+    fn should_heigher(&self, i: usize, j: usize) -> bool {
+        let heap = &self.heap;
+        heap[i] <= heap[j]
+    }
+}
+
+fn parent_of(idx: usize) -> usize {
+    (idx - 1) / 2
+}
+fn lchild_of(idx: usize) -> usize {
+    idx * 2 + 1
+}
+fn rchild_of(idx: usize) -> usize {
+    idx * 2 + 2
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,59 @@ pub type HeapResult = Result<(), ()>;
 pub trait Heap<T: Ord> {
     fn push(&mut self, val: T) -> HeapResult;
     fn pop(&mut self) -> Option<T>;
+    fn is_empty(&self) -> bool;
+}
+
+#[cfg(test)]
+mod heap {
+    use super::*;
+
+    fn is_empty_tester(uut: &dyn Heap<i32>) {
+        assert!(uut.is_empty());
+    }
+    fn isnt_empty_tester(uut: &mut dyn Heap<i32>) {
+        uut.push(1).expect("Push failed");
+        assert!(!uut.is_empty());
+    }
+    fn push_and_pop_min_tester(uut: &mut dyn Heap<i32>) {
+        let mut values = [5, 3, 2, 4, 1];
+        for v in values.iter() {
+            uut.push(*v).expect("Push failed");
+        }
+        values.sort();
+        for v in values.iter() {
+            let poped = uut.pop().expect("Out of value");
+            assert_eq!(*v, poped);
+        }
+    }
+    #[cfg(test)]
+    mod binary_heap {
+        use crate::binary::BinaryHeap;
+        use super::*;
+        #[test]
+        fn is_empty_max() {
+            let uut = BinaryHeap::new_max();
+            is_empty_tester(&uut);
+        }
+        #[test]
+        fn is_empty_min() {
+            let uut = BinaryHeap::new_min();
+            is_empty_tester(&uut);
+        }
+        #[test]
+        fn isnt_empty_max() {
+            let mut uut = BinaryHeap::new_max();
+            isnt_empty_tester(&mut uut);
+        }
+        #[test]
+        fn isnt_empty_min() {
+            let mut uut = BinaryHeap::new_min();
+            isnt_empty_tester(&mut uut);
+        }
+        #[test]
+        fn push_and_pop_min() {
+            let mut uut = BinaryHeap::new_min();
+            push_and_pop_min_tester(&mut uut);
+        }
+    }
 }
